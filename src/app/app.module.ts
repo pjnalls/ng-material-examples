012222f2577e1a6dabae3ab91693834717a53f91
Preston <preston.james.nalls@gmail.com>
2021-04-11 16:27:51 -0500

    Reference necessary modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,24 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 /** */
 
+/** Modules needed for 'form' example: */
+import { FormComponent } from './form/form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+/** */
+
 @NgModule({
   declarations: [
     AppComponent,
-    TableSortAndPaginationComponent
+    TableSortAndPaginationComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
@@ -24,11 +38,27 @@ import { MatSortModule } from '@angular/material/sort';
 
     /** Modules needed for 
      * 'table-sort-and-pagination' 
-     * example: */
+     * example: 
+     * */
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     /** */
+
+    /** Modules needed for
+     * 'form'
+     * example:
+     * */
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRadioModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
